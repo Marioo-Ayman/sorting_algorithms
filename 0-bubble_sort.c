46c69d980af77c0dc340a9ba6dc1a3649c877ccb
RafikSameh <rafiksameh123@gmail.com>
2023-07-25 15:04:40 -0700

    0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - is a bubblesort function
+ * @array: is the sorted array
+ * @size: size of array
+ * Return: nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    size_t len = size;
+    size_t temp;
+    for (i = 0; i < len-1; i++)
+    {
+        for (j = 0; j < len-i-1; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+            temp=array[j];
+            array[j]=array[j+1];
+            array[j+1]=temp;
+            }
+           print_array(array, size);
+        }
+    }
+}
