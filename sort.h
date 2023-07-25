28977c9f913df6ab966a2feb4d49abab6a58913c
RafikSameh <rafiksameh123@gmail.com>
2023-07-25 15:27:19 -0700

    fixing

--- sort.h
+++ sort.h
@@ -4,14 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* functions */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,9 +13,17 @@ void quick_sort(int *array, size_t size);
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
+
+/* functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
